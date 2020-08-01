932022e1cc8a6ffe9c21e52d3cae1705ee8a0c70
Alex Wild <alex.j.wild@gmail.com>
2020-08-01 17:26:13 +0100

    Filter home page table with tabs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import { capitalCase } from 'change-case';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import Layout from '../components/layout';
 import Table from '../components/table';
+import { AREA_TYPES } from '../constants';
 
 const NameCell = ({ row, value }) => (
   <Link to={row.original.slug}>{value}</Link>
@@ -37,11 +39,17 @@ const Home = () => {
     }
   `);
 
+  const [areaTypeFilter, setAreaTypeFilter] = useState(null);
+  const handleTabClick = (event, areaType) => {
+    event.preventDefault();
+    setAreaTypeFilter(areaType);
+  };
+
   const columns = useMemo(
     () => [
       {
         Header: 'Position',
-        accessor: 'rank',
+        accessor: 'position',
       },
       {
         Header: 'Name',
@@ -68,14 +76,40 @@ const Home = () => {
     []
   );
 
-  const data = useMemo(() => allLocation.edges.map(({ node }) => node), [
-    allLocation.edges,
-  ]);
+  const data = useMemo(
+    () =>
+      allLocation.edges
+        .filter(
+          ({ node }) => !areaTypeFilter || areaTypeFilter === node.areaType
+        )
+        .sort(({ node: a }, { node: b }) => a.rank - b.rank)
+        .map(({ node }, index) => ({ ...node, position: index + 1 })),
+    [allLocation.edges, areaTypeFilter]
+  );
   console.log('data', data);
 
   return (
     <Layout>
       <h1>Home</h1>
+      <nav className="tabs">
+        <a
+          className={areaTypeFilter ? null : 'active'}
+          href="#"
+          onClick={(event) => handleTabClick(event, null)}
+        >
+          All
+        </a>
+        {AREA_TYPES.map((areaType) => (
+          <a
+            key={areaType}
+            className={areaTypeFilter === areaType ? 'active' : null}
+            href="#"
+            onClick={(event) => handleTabClick(event, areaType)}
+          >
+            {capitalCase(areaType)}
+          </a>
+        ))}
+      </nav>
       <Table columns={columns} data={data} />
     </Layout>
   );
