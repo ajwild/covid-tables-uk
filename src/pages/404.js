9ad8c7d28aaf870faab982b4d2d60db38e7a8762
Alex Wild <alex.j.wild@gmail.com>
2020-08-01 14:36:15 +0100

    Add custom 404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const Error404 = () => (
+  <Layout>
+    <h1>404 Error</h1>
+  </Layout>
+);
+
+export default Error404;
