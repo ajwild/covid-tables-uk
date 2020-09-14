8ca565c3d787c7cf670abd49d115e988236cc4cf
Alex Wild <alex.j.wild@gmail.com>
2020-09-14 18:58:59 +0100

    Add unit tests for cache util

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: '',
   },
-  moduleDirectories: ['node_modules', 'utils'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -11,7 +10,7 @@ module.exports = {
   },
   setupFiles: ['<rootDir>/loadershim.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['.cache', 'cypress', 'node_modules', 'public'],
+  testPathIgnorePatterns: ['\\.cache', 'cypress', 'node_modules', 'public'],
   transform: {
     '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
   },
---
+++ src/utils/__tests__/cache.ts
@@ -0,0 +1,53 @@
+import { Reporter } from 'gatsby';
+
+import { tryCacheWithFallback } from '../cache';
+
+describe('cache', () => {
+  const mockedGatsbyCache = {
+    get: jest.fn(),
+    set: jest.fn(),
+  };
+  const cacheKey = 'location';
+  const mockedReporter = ({ info: jest.fn() } as unknown) as Reporter;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('tryCacheWithFallback', () => {
+    const fallbackFn = jest.fn();
+    const fallbackArgs = ['test'];
+
+    it('Calls fallback function with arguments when the cache is empty', async () => {
+      const returnValue = ['hello', 'world'];
+      fallbackFn.mockReturnValue(returnValue);
+
+      const data = await tryCacheWithFallback(
+        [mockedGatsbyCache, cacheKey, mockedReporter],
+        fallbackFn,
+        fallbackArgs
+      );
+
+      expect(fallbackFn).toHaveBeenCalledWith(...fallbackArgs);
+      expect(data).toEqual(returnValue);
+    });
+
+    it('Uses the cache when available', async () => {
+      const returnValue = ['hello', 'world'];
+      mockedGatsbyCache.get.mockReturnValue({
+        args: JSON.stringify(fallbackArgs),
+        created: Date.now(),
+        data: returnValue,
+      });
+
+      const data = await tryCacheWithFallback(
+        [mockedGatsbyCache, cacheKey, mockedReporter],
+        fallbackFn,
+        fallbackArgs
+      );
+
+      expect(fallbackFn).not.toHaveBeenCalled();
+      expect(data).toEqual(returnValue);
+    });
+  });
+});
